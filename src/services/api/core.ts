40ea0499c1416ebf24f95f6b4d211687bc4444c9
Dmytro Halaiskiy <dmytrohalaiskiy@dao-group.org>
2023-04-12 16:46:51 +0300

    CHANGED: Api url

--- src/services/api/core.ts
+++ src/services/api/core.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL:
-    process.env.API_URL || 'https://react-chat-cod4-app.herokuapp.com/',
+    process.env.API_URL || 'http://localhost:3001/api/v1',
   withCredentials: true,
 });
 
