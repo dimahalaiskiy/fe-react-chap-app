41d5c1ad5a721bc3b59f22d02d3cfa9907cd79cd
Dmytro Halaiskiy <dmytrohalaiskiy@dao-group.org>
2023-04-13 19:22:04 +0300

    CHENGED: Api url

--- src/services/api/core.ts
+++ src/services/api/core.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL:
-    process.env.API_URL || 'http://localhost:3001/api/v1',
+    process.env.API_URL || 'https://react-chap-app.vercel.app/api/v1',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
