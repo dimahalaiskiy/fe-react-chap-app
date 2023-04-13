5858fd584e49e3d5c1fcf2e02bf8355eb6fcca33
Dmytro Halaiskiy <dmytrohalaiskiy@dao-group.org>
2023-04-13 19:24:53 +0300

    CHENGED: Api url

--- src/services/api/core.ts
+++ src/services/api/core.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL:
-    process.env.API_URL || 'https://react-chap-app.vercel.app/api/v1',
+    process.env.API_URL || 'https://react-chat-cod4-app.herokuapp.com/api/v1',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
