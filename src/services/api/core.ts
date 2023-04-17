45bfe76bf57992ff0590481402d8abd68899223e
Dmytro Halaiskiy <dmytrohalaiskiy@dao-group.org>
2023-04-17 17:28:19 +0300

    ADDED: httpAgent to axios

--- src/services/api/core.ts
+++ src/services/api/core.ts
@@ -1,13 +1,20 @@
 import axios, { AxiosInstance } from 'axios';
+import https from 'https';
+
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL:
     process.env.API_URL ||
     'https://react-chat-cod4-app.herokuapp.com/api/v1',
   withCredentials: true,
+  httpAgent: agent,
   headers: {
     'Content-Type': 'application/json',
     'X-Requested-With': 'XMLHttpRequest',
+    SameSite: 'none',
   },
 });
 
