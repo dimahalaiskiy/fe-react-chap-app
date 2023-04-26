f30db038dead9fe1b3f046ec1d92b2b90d2611be
Dmytro Halaiskiy <dmytrohalaiskiy@dao-group.org>
2023-04-26 10:57:00 +0300

    ADDED: Mode types in tsconfig

--- src/services/api/core.ts
+++ src/services/api/core.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosInstance } from 'axios';
-import { process } from 'node';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: process.env.API_URL || 'http://localhost:3002/api/v1',
